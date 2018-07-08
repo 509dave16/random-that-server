44e92d5be2367361f1e3287dc9f1b968592ebc59
David Fall <davidfall@rocketmail.com>
2018-07-08 00:00:14 -0700

    RTS : Setup server for level down storage of data

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 'use strict';
 
-var Gun = require('gun');
 var http = require('http');
 var port = process.env.PORT || 8081;
 var fs = require('fs');
+var Gun = require('gun');
+require('gun-level');
+
+// Import the required libraries
+const levelup = require('levelup');
+const encode = require('encoding-down');
+const leveldown = require('leveldown');
+
+// Create a new level instance which saves
+// to the `data/` folder.
+const levelDB = levelup(
+	encode(
+		leveldown('data'),
+		{ valueEncoding: 'json' }
+	)
+)
 
 // Listens on /gun.js route.
 var server = http.Server();
@@ -11,13 +26,11 @@ var server = http.Server();
 // Serves up /index.html
 server.on('request', function (req, res) {
 	if(Gun.serve(req, res)){ return }
-	// if (req.url === '/' || req.url === '/index.html') {
-	// 	fs.createReadStream('index.html').pipe(res);
-	//}
 });
 
 var gun = Gun({
-	file: 'data.json', // Saves all data to `data.json`.
+	// file: 'data.json', // Saves all data to `data.json`.
+	level: levelDB,
 	web: server // Handles real-time requests and updates.
 });
 
