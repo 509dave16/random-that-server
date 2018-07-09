37d4d5073487f5c367d412100630c760f4d3b941
David Fall <davidfall@rocketmail.com>
2018-07-09 00:44:18 -0700

    Switching to Gun Radix

--- server.js
+++ server.js
@@ -1,37 +1,18 @@
 'use strict';
 
 var http = require('http');
-var port = process.env.PORT || 8081;
+var port = process.env.PORT || 8080;
 var fs = require('fs');
 var Gun = require('gun');
-require('gun-level');
-
-// Import the required libraries
-const levelup = require('levelup');
-const encode = require('encoding-down');
-const leveldown = require('leveldown');
-
-// Create a new level instance which saves
-// to the `data/` folder.
-const levelDB = levelup(
-	encode(
-		leveldown('data'),
-		{ valueEncoding: 'json' }
-	)
-)
-
-// Listens on /gun.js route.
 var server = http.Server();
 
-// Serves up /index.html
 server.on('request', function (req, res) {
 	if(Gun.serve(req, res)){ return }
 });
 
 var gun = Gun({
-	// file: 'data.json', // Saves all data to `data.json`.
-	level: levelDB,
-	web: server // Handles real-time requests and updates.
+	localStorage: false,
+	web: server
 });
 
 server.listen(port, function () {
